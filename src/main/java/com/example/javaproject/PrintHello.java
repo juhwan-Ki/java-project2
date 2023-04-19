143b3eb6e3788cdf56016d6b991be28844006f49
juhwan <kjhw990218@gmail.com>
2023-04-19 11:16:37 +0900

    feat: first commit - java Project 빌드

---
+++ src/main/java/com/example/javaproject/PrintHello.java
@@ -0,0 +1,7 @@
+package com.example.javaproject;
+
+public class PrintHello {
+    public void print(){
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/example/javaproject/PrintHelloTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject;
+
+import com.example.javaproject.PrintHello;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello pr = new PrintHello(); // 인스터스화
+        pr.print();
+    }
+}
