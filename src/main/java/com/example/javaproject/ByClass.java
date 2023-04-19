627811477be096aacdd13b1e1434d4f207a617a0
juhwan <kjhw990218@gmail.com>
2023-04-19 11:30:55 +0900

    feat: Bye기능 추가 - Bye Class, Bye Class Test 추가

---
+++ src/main/java/com/example/javaproject/ByClass.java
@@ -0,0 +1,7 @@
+package com.example.javaproject;
+
+public class ByClass {
+    public void print(){
+        System.out.println("Bye");
+    }
+}
---
+++ src/main/java/com/example/javaproject/ByClassTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject;
+
+public class ByClassTest {
+    public static void main(String[] args) {
+        ByClass by = new ByClass();
+        by.print();
+    }
+}
