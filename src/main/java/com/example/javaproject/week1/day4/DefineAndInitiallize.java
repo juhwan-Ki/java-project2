9581d2fcf511bb172aeeb225641ce04f84e64d8e
juhwan <kjhw990218@gmail.com>
2023-04-20 16:02:30 +0900

    feat: 변수 초기화 추가 - 변수 초기화 실습 클래스 추가

---
+++ src/main/java/com/example/javaproject/week1/day4/DefineAndInitiallize.java
@@ -0,0 +1,10 @@
+package com.example.javaproject.week1.day4;
+
+import com.example.javaproject.PrintHello;
+
+public class DefineAndInitiallize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+    }
+}
