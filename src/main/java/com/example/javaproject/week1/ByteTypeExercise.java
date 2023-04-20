714568fd35f6c8dc1c79ae4f676602047b2de5b0
juhwan <kjhw990218@gmail.com>
2023-04-20 14:40:41 +0900

    feat: byte, 변수값 추가 - byte, 변수 선언 클래스 추가

---
+++ src/main/java/com/example/javaproject/week1/ByteTypeExercise.java
@@ -0,0 +1,10 @@
+package com.example.javaproject.week1;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -128;
+//        byte byte3 = (byte) -129;  byte는 -128까지만 담김
+        System.out.println(byte1);
+    }
+}
