15d25e3c96f47c04d9fc3c3a8c5a89ef76a33371
juhwan <kjhw990218@gmail.com>
2023-04-20 09:26:21 +0900

    feat: 숫자 2개 받아서 더하는 기능 및 코드업 10번까지 풀이 추가 - ScannerEx, ScannerExTest, Codeup1001~ Codeup1010 추가

---
+++ src/main/java/com/example/javaproject/week1/ScannerExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject.week1;
+
+import java.util.Scanner;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+}
