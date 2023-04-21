0818a57e8f8b55a00a0597f3fdcfb1407df8a6f4
juhwan <kjhw990218@gmail.com>
2023-04-21 11:05:25 +0900

    Feat: Codeup1036 풀이 추가 - Codeup1036 풀이 클래스 추가

---
+++ src/main/java/com/example/javaproject/codeup/Codeup1036.java
@@ -0,0 +1,12 @@
+package com.example.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sVal = sc.next();
+        int iVal = sVal.charAt(0);
+        System.out.print(iVal);
+    }
+}
