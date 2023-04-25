824b6c23820ade340d18a0bbc98d25b040738e2f
juhwan <kjhw990218@gmail.com>
2023-04-25 15:30:48 +0900

    Feat: Codeup1041 풀이 추가 - Codeup1041 까지 풀이 추가

---
+++ src/main/java/com/example/javaproject/codeup/Codeup1041.java
@@ -0,0 +1,11 @@
+package com.example.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.next().charAt(0) + 1;
+        System.out.println((char) iVal);
+    }
+}
