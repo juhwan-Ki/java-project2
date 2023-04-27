0f3557fd789cc75ea90043a4c8586c73aba4382f
juhwan <kjhw990218@gmail.com>
2023-04-27 14:24:08 +0900

    Feat: 비트연산, Codeup1047~48 풀이 추가 - 비트연산, Codeup1047~48 풀이 클래스 추가

---
+++ src/main/java/com/example/javaproject/codeup/Codeup1047.java
@@ -0,0 +1,12 @@
+package com.example.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%d", num << 1);
+
+    }
+}
