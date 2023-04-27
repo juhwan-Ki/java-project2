b08e2c90fd9f56704c5831b417a0085cf70d27a4
juhwan <kjhw990218@gmail.com>
2023-04-27 13:18:23 +0900

    Feat: Codeup1053 ~ 1056 풀이 추가 - Codeup1053 ~ 1056 풀이 클래스 추가

---
+++ src/main/java/com/example/javaproject/codeup/Codeup1053.java
@@ -0,0 +1,11 @@
+package com.example.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        System.out.println(val == 0? 1 : 0);
+    }
+}
---
+++ src/main/java/com/example/javaproject/codeup/Codeup1054.java
@@ -0,0 +1,15 @@
+package com.example.javaproject.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1054 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] twoNum = sc.nextLine().split(" ");
+        if(Integer.parseInt(twoNum[0]) == 1 && Integer.parseInt(twoNum[1]) == 1){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
