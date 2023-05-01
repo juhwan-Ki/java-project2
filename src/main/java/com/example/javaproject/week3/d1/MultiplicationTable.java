c5ae1dcba91ed492f9288ae9130d067dee814e8b
juhwan <kjhw990218@gmail.com>
2023-05-01 14:40:44 +0900

    Feat: 특정단 출력 - 특정 단 출력

--- src/main/java/com/example/javaproject/week3/d1/MultiplicationTable.java
+++ src/main/java/com/example/javaproject/week3/d1/MultiplicationTable.java
@@ -1,16 +1,24 @@
 package com.example.javaproject.week3.d1;
 
 public class MultiplicationTable {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++) {
-            System.out.printf("2 * %d = %d\n", i, i * 2);
-        }
 
-        for(int i = 2; i <= 4; i++){
-            for(int j = 1; j <= 9; j++){
-                System.out.printf("%d * %d = %d\n", i, j, j * i);
-            }
-            System.out.println("=============");
+    public static void printDan(int dan){
+        for(int j = 1; j <= 9; j++){
+            System.out.printf("%d * %d = %d\n", dan, j, j * dan);
         }
+        System.out.println("=============");
+    }
+
+    public static void main(String[] args) {
+//        for(int i = 2; i <= 4; i++){
+//            for(int j = 1; j <= 9; j++){
+//                System.out.printf("%d * %d = %d\n", i, j, j * i);
+//            }
+//            System.out.println("=============");
+//        }
+        printDan(2);
+        printDan(4);
+        printDan(5);
+        printDan(8);
     }
 }
