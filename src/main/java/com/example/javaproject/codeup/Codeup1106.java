1992c42dbc8cfe03786645825e17ba361e5357f2
juhwan <kjhw990218@gmail.com>
2023-05-01 09:08:15 +0900

    Feat: Codeup1106까지 추가 - Codeup1106까지 추가

---
+++ src/main/java/com/example/javaproject/codeup/Codeup1106.java
@@ -0,0 +1,7 @@
+package com.example.javaproject.codeup;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
+    }
+}
