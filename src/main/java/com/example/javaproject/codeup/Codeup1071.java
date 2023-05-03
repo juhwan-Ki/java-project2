b49e12180a2cf08d0152a5b0cbff06516513bdda
juhwan <kjhw990218@gmail.com>
2023-05-03 10:09:24 +0900

    Fix: Codeup1071, 73 do while로 변경 - while에서 do while로 변경

--- src/main/java/com/example/javaproject/codeup/Codeup1071.java
+++ src/main/java/com/example/javaproject/codeup/Codeup1071.java
@@ -6,13 +6,19 @@ public class Codeup1071 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            int val = sc.nextInt();
-            if (val == 0) {
-                return;
-            } else {
-                System.out.println(val);
-            }
-        }
+//        while (true) {
+//            int val = sc.nextInt();
+//            if (val == 0) {
+//                return;
+//            } else {
+//                System.out.println(val);
+//            }
+//        }
+        // do while로 풀기
+        int num;
+        do {
+            num = sc.nextInt();
+            System.out.println(num);
+        } while (num != 0);
     }
 }
